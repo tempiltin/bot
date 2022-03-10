955a77fabc6d4bcd90b25773e00856519003c439
Tempiltin99 <temurbekshukurov0707@gmail.com>
2022-03-10 15:20:22 +0500

    upload

--- app.js
+++ app.js
@@ -58,18 +58,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-
-
-
-
-
-
-
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
